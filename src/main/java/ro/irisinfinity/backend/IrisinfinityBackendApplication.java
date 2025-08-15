4afd3639b2c04e5fdca06a6e93d005d01fb00861
Nistor Gheorghe <nistorgeorge404@gmail.com>
2025-08-15 19:39:09 +0300

    Project init

---
+++ src/main/java/ro/irisinfinity/backend/IrisinfinityBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.irisinfinity.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisinfinityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisinfinityBackendApplication.class, args);
+    }
+
+}
